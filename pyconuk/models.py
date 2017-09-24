5efceae383c0896a25a9318d0903eb0402017bcc
Alex Chan <alex@alexwlchan.net>
2017-09-24 22:11:42 +0100

    Add a show_sponsors field to pages

--- pyconuk/models.py
+++ pyconuk/models.py
@@ -8,6 +8,7 @@ from django_amber.models import ModelWithContent, ModelWithoutContent
 class Page(ModelWithContent):
     title = models.CharField(max_length=100)
     template = models.CharField(max_length=20, default='page.html')
+    show_sponsors = models.BooleanField(default=True)
 
     dump_dir_path = 'pages'
 
