b9b0e4bc585b700db8a63168ec46f9d5b8ac1ce3
Emmanuelle Delescolle <emma@levit.be>
2017-10-01 11:29:56 +0200

    Typo fix

--- pyconuk/views.py
+++ pyconuk/views.py
@@ -57,7 +57,7 @@ def unlinked_pages(request):
         '/kids/',
         '/pinner-award/',
         '/sponsors/',
-        '/transcode/,
+        '/transcode/',
     ]
 
     for redirection in Redirection.objects.order_by('key'):
